78e1a0f75c6f2cfa69bb8875c89aeb2002dff479
sid-01 <60143050+sid-01@users.noreply.github.com>
2020-04-26 14:38:04 +0530

    publishing first page

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="media" >
+  <img src="https://gec-chamarajanagara.ac.in/wp-content/themes/Gecc_Umbrella/assets/images/Kar-Logo.png"  class="mr-3" alt="..." height="70px" width="85px">
+  <div class="media-body">
+    <h5 class="mt-0">Department of Computer Science</h5>
+<p><a href="https://gec-chamarajanagara.ac.in/" style="text-decoration:none">Government engineering college Chamarajanagar</a></p>
+<p style="font-size:10pt;">Affiliated to VTU Belagavi-59001</p>
+  </div>
+</div>
+    </div>
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon "><i class="fas fa-bars" style="color:#fff; font-size:28px;"></i></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Admin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Gallery</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Conatct Us</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          More
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="padding:0;background-color:black;">
+          <a class="dropdown-item" style="margin-top:0.3cm;" href="#">Notes</a>
+          <a class="dropdown-item"  style="margin-top:0.3cm;" href="#">Staff</a>
+          <a class="dropdown-item" style="margin-top:0.3cm;" href="#">Softwares</a>
+        </div>
+      </li>
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+</ol>
+<div class="carousel-inner">
+<div class="carousel-item active">
+  <img src="https://gec-chamarajanagara.ac.in/wp-content/uploads/2020/03/DSC_0042.jpg" class="d-block w-100" alt="...">
+</div>
+<div class="carousel-item">
+  <img src="images\IMG-20190410-WA0020.jpg" class="d-block w-100" alt="...">
+</div>
+<div class="carousel-item">
+  <img src="images\IMG-20190410-WA0074.jpg" class="d-block w-100" alt="...">
+</div>
+<div class="carousel-item">
+  <img src="images\farewell gp photo.jpg" class="d-block w-100" alt="...">
+</div>
+<div class="carousel-item">
+  <img src="images\IMG_20190525_163051.jpg" class="d-block w-100" alt="...">
+</div>
+</div>
+<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+<span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+<span class="carousel-control-next-icon" aria-hidden="true"></span>
+<span class="sr-only">Next</span>
+</a>
+</div><br><br>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6">
+      <h2 style="margin-top:1rem!important" class="text-center">About Department</h2><br>
+      <p style="text-indent:1rem">Department of Computer Science and Engineering was started in the year 2007. The department has well equipped laboratories with latest configuration and software’s, and has experienced and highly qualified teaching and technical staff members</p>
+
+<p>Department maintains separate library and the innovative cell for the students and staff for their knowledge thrust and shaping the students innovativeness.</p>
+    </div>
+    <div class="col-lg-6">
+      <img src="https://gec-chamarajanagara.ac.in/wp-content/uploads/2018/06/cs.jpg" alt="" width="100%" height="300px">
+
+    </div>
+
+  </div><br><br><br>
+
+  <div class="row padding">
+    <div class="col-lg-6">
+      <div class="card mx-auto" style="width: 22rem;">
+          <img class="card-img-top" height="200px!important" src="https://gec-chamarajanagara.ac.in/wp-admin/faculty_photo/Dr.%20Nagappu%20U%20Bhajantri.png" alt="Card image cap">
+             <div class="card-body">
+                  <h5 class="card-title text-center">Dr.Nagappa U Bhajantri</h5>
+                     <p class="card-text text-center" style="color:grey"><b>Head of Department</b></p>
+                      <p class="card-text text-center">Department of Computer Science</p>
+                      <a href="#" class="btn btn-primary" style="margin-left:110px">Profile</a>
+             </div>
+       </div>
+
+    </div>
+  <div class="col-lg-6">
+      <h2 class="mt-3 text-center">About HOD</h2><br>
+      <p style="line-height:26px;text-indent:1rem">Department also motivate the students to show their extraordinary talent, creativity, innovative through students forum. Department encourages alumni’s interaction programs which will help the pursuing students while gathering industry needs.</p>
+
+    </div>
+
+  </div></div>
+
+<br><br>
+<div class="container">
+  <table class="table table-bordered">
+    <th style="background-color:lightgrey">Objectives</th>
+    <tr>
+      <td>
+<table class="table table-bordered" style="margin:0.5rem">
+  <tr>
+    <td>To develop the department into center for excellence.</td>
+  </tr>
+  <tr>
+    <td>Providing World Class Technical Education in the field of Computer as per futuristic global technical man power requirements. </td>
+  </tr>
+  <tr>
+    <td>To establish R&D center with a pool of experts.</td>
+  </tr>
+</table>
+
+
+      </td>
+    </tr>
+  </table>
+
+</div>
+
+<div class="container-fluid">
+  <table class="table table-bordered table1">
+    <tr>
+      <td>
+        <table class="table table-bordered table2">
+          <tr>
+      <td class="text-white"><b>"The way to build a nation is to build a good citizen. The majority of the citizens should be efficient, of good character and possess a reasonable high sense of duty."</b>
+          <p class="float-right bg-success" style="padding:0.2cm;">- Sir M Visvesvaraya </p>
+      </td></tr></table></td>
+    </tr>
+
+  </table>
+
+</div>
+<footer>
+
+
+
+</footer>
+
+
+
+  </body>
+</html>
